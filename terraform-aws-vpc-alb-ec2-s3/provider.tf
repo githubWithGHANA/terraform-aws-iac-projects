508fba64853a6527ac1fa3c86b4382dd247aed4e
Ghanashyama  Mahunta <ghanshyammahunta@gmail.com>
2026-01-02 18:03:25 +0530

    Separated Version part

--- terraform-aws-vpc-alb-ec2-s3/provider.tf
+++ terraform-aws-vpc-alb-ec2-s3/provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "6.27.0"
-    }
-  }
-}
-
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
